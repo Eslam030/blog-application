75a7ed3af331ab19cb372621791a9f028422d10c
Eslam Sayed <esla889900@gmail.com>
2023-09-27 00:38:12 +0300

    final version

--- webapp/blog_application/models.py
+++ webapp/blog_application/models.py
@@ -18,14 +18,19 @@ class Post (models.Model):
 
 
 class comment (models.Model):
-    post_id = models.ForeignKey(
+    post = models.ForeignKey(
         Post,
         on_delete=models.CASCADE
     )
-    user_id = models.ForeignKey(
+    user = models.ForeignKey(
         User,
         on_delete=models.CASCADE
     )
     comment_content = models.CharField(max_length=255)
+    publish_data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment_content
+
 
 # Create your models here.
--- webapp/blog_application/views.py
+++ webapp/blog_application/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegisterForm
 from django.http import HttpResponse
-from django.views.generic import ListView , DetailView
-from .models import Post
+from django.views.generic import ListView, DetailView
+from .models import Post, comment
 
 # Create your views here.
 
@@ -12,7 +12,7 @@ class Index(ListView):
     model = Post
     queryset = Post.objects.all().order_by('-publish_date')
     template_name = 'blog/index.html'
-    paginate_by = 1
+    paginate_by = 3
 
 
 class RegisterView(View):
@@ -27,7 +27,7 @@ class RegisterView(View):
             form.save()
             return redirect('index')
         else:
-            return HttpResponse('Wrong from')
+            return redirect('index')
 
 
 class createView (View):
@@ -41,9 +41,16 @@ class createView (View):
         print(title, status, content)
         Post.objects.create(title=title, statue=status,
                             content=content, owner=request.user)
-        return render(request, 'blog/index.html')
-    
+        return redirect('index')
+
 
 class DetailPostView(DetailView):
-    model = Post
-    template_name = 'blog/blog_post.html'
+    def get(self, request, pk):
+        comments = comment.objects.all().filter(post_id=pk)
+        post = Post.objects.all().filter(id=pk)
+        return render(request, 'blog/blog_post.html', {'comments': comments, 'post': post[0]})
+
+    def post(self, request, pk):
+        comment.objects.create(
+            post_id=pk, comment_content=request.POST['comment_field'], user_id=request.user.id, )
+        return redirect('index')
