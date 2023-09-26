8e38f8cbe1b0fed903918b7480ab47162bc944ff
SAMarSAMir2 <102358763+SAMarSAMir2@users.noreply.github.com>
2023-09-26 14:19:25 +0300

    templates

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ----------------------------------------------
+
+LOGIN_REDIRECT_URL = 'index'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
